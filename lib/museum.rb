64e8d51a86bda73eee7d901a345fb2ad8f0689b7
Eli Fuchsman <efuchsman@gmail.com>
2022-09-21 10:37:44 -0600

    Complete IT2

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,9 +1,21 @@
 class Museum
 
-attr_reader :name
-def initialize(name)
-  @name = name
+attr_reader :name, :exhibits
+  def initialize(name)
+    @name = name
+    @exhibits = []
+  end
 
-end
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+
+  def recommend_exhibits(patron)
+    recommended = @exhibits.select do |exhibit|
+      exhibit if patron.interests.include?(exhibit.name)
+    end
+    p recommended
+  end
 
 end
